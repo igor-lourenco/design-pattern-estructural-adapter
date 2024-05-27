fe8111f499beafdbec841a491c2fe3b37aa25291
igor-lourenco <igor.lourencosantos@gmail.com>
2024-05-27 11:30:44 -0300

    Implementacao da classe Client.

--- src/main/java/estructural/adapter/tvPort/Client.java
+++ src/main/java/estructural/adapter/tvPort/Client.java
@@ -19,13 +19,13 @@ public class Client {
 	Computer pc2 = new Computer();
 	OldMonitor monitor = new OldMonitor();
 //	 pc2.connectPort(monitor.getConnector()); // não funciona!  por isso que criamos o adaptador
-	pc2.connectPort(new HDMIToVGAAdapter(monitor));
+	pc2.connectPort(new HDMIToVGAAdapter(monitor)); // implementação usando objeto como parametro
 	pc2.sendImageAndSound("Cat and rainbow", "Nyan cat song");
 
 	System.out.println("------ Monitor Class Adapter ----------");
 
 	Computer pc3 = new Computer();
-	HDMIToOldMonitorAdapter monitorAdaptee = new HDMIToOldMonitorAdapter();
+	HDMIToOldMonitorAdapter monitorAdaptee = new HDMIToOldMonitorAdapter(); // implementação usando classe com herança como parametro
 	pc3.connectPort(monitorAdaptee);
 	pc3.sendImageAndSound("Cat and rainbow", "Nyan cat song");
     }
